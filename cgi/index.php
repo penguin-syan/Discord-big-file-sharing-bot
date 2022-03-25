36e6e69eb771413a9df051a323c96d42e855e328
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-03-25 23:14:08 +0900

    update

--- cgi/index.php
+++ cgi/index.php
@@ -1,16 +1,16 @@
 <?php
 require_once 'upload.php';
 
-if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
-    $upload_file = date("Ymd-His").$_FILES['upfile']['name'];
-    if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "../files/".$upload_file)) {
-        chmod("../files/".$upload_file, 0644);
-        echo "メニューを追加しました．";
-    } else {
-        echo "ファイルをアップロードできません。";
-        return;
-    }
-} else {
-    echo "ファイルが選択されていません。";
-    return;
-}
+// if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
+//     $upload_file = date("Ymd-His").$_FILES['upfile']['name'];
+//     if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "../files/".$upload_file)) {
+//         chmod("../files/".$upload_file, 0644);
+//         echo "メニューを追加しました．";
+//     } else {
+//         echo "ファイルをアップロードできません。";
+//         return;
+//     }
+// } else {
+//     echo "ファイルが選択されていません。";
+//     return;
+// }
