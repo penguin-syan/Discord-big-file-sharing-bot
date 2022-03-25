1c5bbec0a54983b3afbdabf974967dab826fd01c
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-03-25 22:21:58 +0900

    ファイルを追加

---
+++ cgi/upload.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+
+<html>
+   <form method="POST" enctype="multipart/form-data" action="index.php">
+      <input type="file" name="upload_file" size="30" id="upload_file">
+      <input type="submit" name="upload_submit" value="追加">   
+   </form>
+</html>
