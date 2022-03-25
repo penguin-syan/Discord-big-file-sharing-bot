fa416e1ea731692546a3dcdca0ee2910b4cd26bf
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-03-25 23:13:39 +0900

    update

--- cgi/upload.php
+++ cgi/upload.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <html>
-   <form method="POST" enctype="multipart/form-data" action="uploadFile()">
+   <form method="POST" enctype="multipart/form-data" action="index.php">
       <input type="file" name="upload_file" size="30" id="upload_file">
       <input type="submit" name="upload_submit" value="追加">   
    </form>
