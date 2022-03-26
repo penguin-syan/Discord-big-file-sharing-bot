3d5224876bc526b1c73e2c0f97383ddb14e9d25f
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-03-26 20:55:52 +0900

    test

--- api/index.php
+++ api/index.php
@@ -1,22 +1,24 @@
 <?php
 require_once '../vendor/autoload.php';
-require_once '../vendor/discord/interactions/discord/InteractionResponseType.php';
+//require_once '../vendor/discord/interactions/discord/InteractionResponseType.php';
 //require_once 'vendor/discord/interactions/Interaction.php';
 
-use Discord\Interaction;
-use Discord\InteractionResponseType;
+// use Discord\Interaction;
+// use Discord\InteractionResponseType;
 
-$CLIENT_PUBLIC_KEY = getenv('CLIENT_PUBLIC_KEY');
+// $CLIENT_PUBLIC_KEY = getenv('CLIENT_PUBLIC_KEY');
 
-$signature = $_SERVER['HTTP_X_SIGNATURE_ED25519'];
-$timestamp = $_SERVER['HTTP_X_SIGNATURE_TIMESTAMP'];
-$postData = file_get_contents('php://input');
+// $signature = $_SERVER['HTTP_X_SIGNATURE_ED25519'];
+// $timestamp = $_SERVER['HTTP_X_SIGNATURE_TIMESTAMP'];
+// $postData = file_get_contents('php://input');
 
-if (Interaction::verifyKey($postData, $signature, $timestamp, $CLIENT_PUBLIC_KEY)) {
-  echo json_encode(array(
-    'type' => InteractionResponseType::PONG
-  ));
-} else {
-  http_response_code(401);
-  echo "Not verified";
-}
+// if (Interaction::verifyKey($postData, $signature, $timestamp, $CLIENT_PUBLIC_KEY)) {
+//     echo json_encode(array(
+//     'type' => InteractionResponseType::PONG
+//   ));
+// } else {
+//     http_response_code(401);
+//     echo "Not verified";
+// }
+
+http_response_code(401);
