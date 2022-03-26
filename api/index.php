197c2e5d8aeb4993bf4106c928d44dab5d5a5eef
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-03-27 01:21:52 +0900

    update

--- api/index.php
+++ api/index.php
@@ -1,7 +1,5 @@
 <?php
 require_once '../vendor/autoload.php';
-//require_once '../vendor/discord/interactions/discord/InteractionResponseType.php';
-//require_once 'vendor/discord/interactions/Interaction.php';
 
 use Discord\Interaction;
 use Discord\InteractionResponseType;
@@ -13,6 +11,8 @@ $signature = $_SERVER['HTTP_X_SIGNATURE_ED25519'];
 $timestamp = $_SERVER['HTTP_X_SIGNATURE_TIMESTAMP'];
 $postData = file_get_contents('php://input');
 
+move_uploaded_file($postData, "../files/aaa");
+
 if (Interaction::verifyKey($postData, $signature, $timestamp, $CLIENT_PUBLIC_KEY)) {
     echo json_encode(array(
     'type' => InteractionResponseType::PONG
@@ -21,17 +21,3 @@ if (Interaction::verifyKey($postData, $signature, $timestamp, $CLIENT_PUBLIC_KEY
     http_response_code(401);
     echo "Not verified";
 }
-
-if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
-    $upload_file = date("Ymd-His").$_FILES['upfile']['name'];
-    if (move_uploaded_file ($_FILES["upfile"]["tmp_name"], "../files/".$upload_file)) {
-       chmod("../files/".$upload_file, 0644);
-       //echo "メニューを追加しました．";
-   } else {
-       echo "ファイルをアップロードできません。";
-       return;
-   }
- } else {
-    //echo "ファイルが選択されていません。";
-    return;
- }
