8a563ca1b71477ed2f249e8d2b0c959daa9ca6b1
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-03-28 23:44:28 +0900

    update

--- cgi/index.php
+++ cgi/index.php
@@ -2,12 +2,12 @@
 require_once '../db_connect.php';
 
 switch(checkData($_GET['id'])){
-    case 0: //アップロード済み
-        require_once 'view.php';
-        break;
-    case 1: //削除済み
-        echo "このデータは保存期間を超過したため削除されました。";
-        break;
+    // case 0: //アップロード済み
+    //     require_once 'view.php';
+    //     break;
+    // case 1: //削除済み
+    //     echo "このデータは保存期間を超過したため削除されました。";
+    //     break;
     default:
         require_once 'upload.php';
 }
