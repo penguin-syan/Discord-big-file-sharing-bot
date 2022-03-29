f4a947fed82ff5fe92453991b2f243bec50130b0
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-03-29 13:12:22 +0900

    update

--- cgi/uploaded.php
+++ cgi/uploaded.php
@@ -18,8 +18,8 @@ if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
    }
 
     $upload_file = date("Ymd-His").$_FILES['upfile']['name'];
-    if (move_uploaded_file($_FILES["upfile"]["tmp_name"], $FILEPASS.$upload_file)) {
-        chmod($FILEPASS.$upload_file, 0644);
+    if (move_uploaded_file($_FILES["upfile"]["tmp_name"], FILEPASS.$upload_file)) {
+        chmod(FILEPASS.$upload_file, 0644);
         echo "ファイルをアップロードしました。";
     } else {
         echo "ファイルをアップロードできません。";
