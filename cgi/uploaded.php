488e1c5e8067d73cbf7c2db126e68a15b2d9881e
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-03-29 13:10:56 +0900

    update

--- cgi/uploaded.php
+++ cgi/uploaded.php
@@ -2,30 +2,30 @@
 define("FILEPASS", "");
 
 if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
-   $filetype;
-   switch(mime_content_type($_FILES['upfile']['tmp_name'])){
-      case "image/png":
-      case "image/jpeg":
-      case "image/gif":
-         $filetype = "image";
-         break;
-      case "video/quicktime":
-         break;
-      default:
-         echo "このファイル形式はアップロードが許可されていません。";
-         echo mime_content_type($_FILES['upfile']['tmp_name']);
-         return;
+    $filetype;
+    switch (mime_content_type($_FILES['upfile']['tmp_name'])) {
+        case "image/png":
+        case "image/jpeg":
+        case "image/gif":
+            $filetype = "image";
+            break;
+        case "video/quicktime":
+            break;
+        default:
+            echo "このファイル形式はアップロードが許可されていません。";
+            echo mime_content_type($_FILES['upfile']['tmp_name']);
+            return;
    }
 
-   $upload_file = date("Ymd-His").$_FILES['upfile']['name'];
-   if (move_uploaded_file ($_FILES["upfile"]["tmp_name"], $FILEPASS.$upload_file)) {
-      chmod($FILEPASS.$upload_file, 0644);
-      echo "ファイルをアップロードしました。";
-  } else {
-      echo "ファイルをアップロードできません。";
-      return;
-  }
+    $upload_file = date("Ymd-His").$_FILES['upfile']['name'];
+    if (move_uploaded_file($_FILES["upfile"]["tmp_name"], $FILEPASS.$upload_file)) {
+        chmod($FILEPASS.$upload_file, 0644);
+        echo "ファイルをアップロードしました。";
+    } else {
+        echo "ファイルをアップロードできません。";
+        return;
+    }
 } else {
-   echo "ファイルが選択されていません。";
-   return;
+    echo "ファイルが選択されていません。";
+    return;
 }
