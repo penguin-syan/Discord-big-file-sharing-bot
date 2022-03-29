fe0baf9feb5706cf8560c59ed8b7af77a8ddfead
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-03-29 12:12:04 +0900

    update

--- cgi/uploaded.php
+++ cgi/uploaded.php
@@ -2,6 +2,17 @@
 define("FILEPASS", "");
 
 if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
+   switch(mime_content_type($_FILES['upfile']['tmp_name'])){
+      case "image/png":
+      case "image/jpeg":
+      case "image/gif":
+      case "image/":
+         break;
+      default:
+         echo "このファイル形式はアップロードが許可されていません。";
+         return;
+   }
+
    $upload_file = date("Ymd-His").$_FILES['upfile']['name'];
    if (move_uploaded_file ($_FILES["upfile"]["tmp_name"], $FILEPASS.$upload_file)) {
       chmod($FILEPASS.$upload_file, 0644);
