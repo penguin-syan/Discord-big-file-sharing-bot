471e667bd0df3775a2b91f3af0102eee942821b1
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-04-02 13:09:16 +0900

    update

--- cgi/view.php
+++ cgi/view.php
@@ -4,24 +4,26 @@ require_once '../db_connect.php';
 $sqlResult = getData($_GET['id']);
 $sqlResult = $sqlResult->fetch(PDO::FETCH_BOTH);
 
-if ($sqlResult['del'] == 0) {
-    switch ($sqlResult['filetyle']) {
-        case 1:
-            echo "img";
-            //echo "<img src='".readfile($filepass.$sqlResult['filename']).".>";
-            break;
-        case 2:
-            echo "mov";
-            break;
-        case 3:
-            echo "pdf";
-            break;
-        default:
-            echo "ERROR: ".__LINE__;
-    }
-} else {
-    echo "このデータは保存期間を超過したため削除されました";
-}
+print_r($sqlResult);
+
+// if ($sqlResult['del'] == 0) {
+//     switch ($sqlResult['filetyle']) {
+//         case 1:
+//             echo "img";
+//             //echo "<img src='".readfile($filepass.$sqlResult['filename']).".>";
+//             break;
+//         case 2:
+//             echo "mov";
+//             break;
+//         case 3:
+//             echo "pdf";
+//             break;
+//         default:
+//             echo "ERROR: ".__LINE__;
+//     }
+// } else {
+//     echo "このデータは保存期間を超過したため削除されました";
+// }
 
 echo "<br>view";
 ?>
