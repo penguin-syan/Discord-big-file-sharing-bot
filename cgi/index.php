48386e9162a095101c0da6f088c2e1e71917a605
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-04-12 19:25:46 +0900

    update

--- cgi/index.php
+++ cgi/index.php
@@ -1,13 +1,15 @@
 <?php
 require_once '../db_connect.php';
 
-switch(checkData($_GET['id'])){
-    case 0: //アップロード済み
-        require_once 'view.php';
-        break;
-    case 2:
-        require_once 'upload.php';
-        break;
-    default:
-        echo "ERROE in ".__LINE__;
-}
+
+echo 'view';
+// switch(checkData($_GET['id'])){
+//     case 0: //アップロード済み
+//         require_once 'view.php';
+//         break;
+//     case 2:
+//         require_once 'upload.php';
+//         break;
+//     default:
+//         echo "ERROE in ".__LINE__;
+// }
