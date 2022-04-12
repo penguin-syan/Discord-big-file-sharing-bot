97721f1e0183e6289d5d8f40992329a2c6f82abb
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-04-12 19:38:18 +0900

    update

--- cgi/view.php
+++ cgi/view.php
@@ -1,29 +1,29 @@
 <?php
 require_once '../db_connect.php';
 
-//$sqlResult = getData($_GET['id']);
-//$sqlResult = $sqlResult->fetch(PDO::FETCH_BOTH);
+$sqlResult = getData($_GET['id']);
+$sqlResult = $sqlResult->fetch(PDO::FETCH_BOTH);
 
-//print_r($sqlResult);
+print_r($sqlResult);
 
-// if ($sqlResult['del'] == 0) {
-//     switch ($sqlResult['filetyle']) {
-//         case 1:
-//             echo "img";
-//             //echo "<img src='".readfile($filepass.$sqlResult['filename']).".>";
-//             break;
-//         case 2:
-//             echo "mov";
-//             break;
-//         case 3:
-//             echo "pdf";
-//             break;
-//         default:
-//             echo "ERROR: ".__LINE__;
-//     }
-// } else {
-//     echo "このデータは保存期間を超過したため削除されました";
-// }
+if ($sqlResult['del'] == 0) {
+    switch ($sqlResult['filetyle']) {
+        case 1:
+            echo "img";
+            //echo "<img src='".readfile($filepass.$sqlResult['filename']).".>";
+            break;
+        case 2:
+            echo "mov";
+            break;
+        case 3:
+            echo "pdf";
+            break;
+        default:
+            echo "ERROR: ".__LINE__;
+    }
+} else {
+    echo "このデータは保存期間を超過したため削除されました";
+}
 
 echo "<br>view";
 ?>
