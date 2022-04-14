37e790168d7496db194577586f630e052bb06957
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-04-14 20:43:03 +0900

    update

--- cgi/view.php
+++ cgi/view.php
@@ -10,7 +10,7 @@ if ($sqlResult['del'] == 0) {
     switch ($sqlResult['filetype']) {
         case 1:
             echo "img";
-            //echo "<img src='".readfile($filepass.$sqlResult['filename']).".>";
+            echo "<img src='".$filepass.$sqlResult['filename'].".>";
             header('Content-type: image/png');
             readfile($filepass.$sqlResult['filename']);
             break;
